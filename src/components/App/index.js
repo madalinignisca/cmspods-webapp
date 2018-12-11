32477316a9f8872609ccc743992a1a18e97aa8bb
Madalin Ignisca <madalin.ignisca@gmail.com>
2018-12-11 06:28:57 +0100

    Refactor App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,12 @@
-import React, { Component } from 'react';
-import Clusters from '../Clusters/clusters';
-import Websites from '../Websites/websites';
-import Navigation from '../Navigation';
+import React from 'react';
 import  { FirebaseContext } from '../Firebase';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Websites />
-        <Clusters />
-        <FirebaseContext.Consumer>
-          {firebase => {
-            return <div>I've access to Firebase and render something.</div>;
-          }}
-        </FirebaseContext.Consumer>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <FirebaseContext.Consumer>
+    {firebase => {
+      return <div>I've access to Firebase and render something.</div>;
+    }}
+  </FirebaseContext.Consumer>
+);
+
+export default App;
