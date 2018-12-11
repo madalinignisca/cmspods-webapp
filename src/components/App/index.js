478c34282c88e32a1758c459d94fc5425789181a
Madalin Ignisca <madalin.ignisca@gmail.com>
2018-12-11 07:08:30 +0100

    Initiate landing pages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router } from  'react-router-dom';
-import  { FirebaseContext } from '../Firebase';
+import {
+  BrowserRouter as Router,
+  Route
+} from  'react-router-dom';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import * as ROUTES from '../../constants/routes';
 import Navigation from '../Navigation';
 
 const App = () => (
-  // <FirebaseContext.Consumer>
     <Router>
-      <Navigation />
+      <div>
+        <Navigation />
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGNIN} component={SignInPage} />
+        <Route path={ROUTES.SIGNUP} component={SignUpPage} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
+        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      </div>
     </Router>
-    // {firebase => {
-    //   return <div>I've access to Firebase and render something.</div>;
-    // }}
-  // </FirebaseContext.Consumer>
 );
 
 export default App;
