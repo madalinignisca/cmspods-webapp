ed9c56287d72abebd2b5d2700a6d6e9a6d5f909d
Madalin Ignisca <madalin.ignisca@gmail.com>
2018-12-11 06:48:07 +0100

    Fix App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from  'react-router-dom';
 import  { FirebaseContext } from '../Firebase';
+import Navigation from '../Navigation';
 
 const App = () => (
-  <FirebaseContext.Consumer>
-    {firebase => {
-      return <div>I've access to Firebase and render something.</div>;
-    }}
-  </FirebaseContext.Consumer>
+  // <FirebaseContext.Consumer>
+    <Router>
+      <Navigation />
+    </Router>
+    // {firebase => {
+    //   return <div>I've access to Firebase and render something.</div>;
+    // }}
+  // </FirebaseContext.Consumer>
 );
 
 export default App;
