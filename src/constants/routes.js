43cf81836970ade2df6d40b9f8c77e4764b4349e
Madalin Ignisca <madalin.ignisca@gmail.com>
2018-12-11 06:28:44 +0100

    Add main routes

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const LANDING = '/';
+export const SIGNUP = '/signup';
+export const SIGNIN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/passwd';
