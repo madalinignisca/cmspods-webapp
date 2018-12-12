875094f3bf127825f3f5e942c7bc5755e4fc8c07
Madalin Ignisca <madalin.ignisca@gmail.com>
2018-12-12 17:13:49 +0100

    Remove not necessary import from App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route
